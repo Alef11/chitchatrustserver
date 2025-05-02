502deea6b58ddcd778e3d7f6a0fbe67f5a913027
Alef11 <alex.m.huber@web.de>
2025-05-02 17:41:15 +0200

    Add to user struct methode to convert user into a SQL injectionsafe String

--- src/db/db.rs
+++ src/db/db.rs
@@ -1,6 +1,10 @@
-use std::sync::LazyLock;
-use mysql::*;
 use mysql::prelude::*;
+use mysql::*;
+use std::sync::LazyLock;
+
+use crate::modules::user::user;
+
+static DB_CHARACTER_LIMIT: usize = 100;
 
 // Replace with your actual MariaDB connection URL
 pub static DB_POOL: LazyLock<Pool> = LazyLock::new(|| {
@@ -10,16 +14,29 @@ pub static DB_POOL: LazyLock<Pool> = LazyLock::new(|| {
 
 pub fn create_users_table() -> Result<()> {
     let mut conn = DB_POOL.get_conn()?;
-    conn.query_drop(
+    let query = format!(
         r"CREATE TABLE IF NOT EXISTS users (
             uuid int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            username varchar(255) NOT NULL,
-            password varchar(255) NOT NULL,
-            email varchar(255),
+            username varchar({}) NOT NULL,
+            password varchar({}) NOT NULL,
+            email varchar({}),
             created_at varchar(19) NOT NULL,
             last_online varchar(19) NOT NULL,
-            is_admin boolean NOT NULL DEFAULT false,
-        )"
-    )?;
+            is_admin boolean NOT NULL DEFAULT false
+        )",
+        DB_CHARACTER_LIMIT, DB_CHARACTER_LIMIT, DB_CHARACTER_LIMIT
+    );
+
+    conn.query_drop(query)?;
+    Ok(())
+}
+
+pub fn insert_user(user: &user) -> Result<()> {
+    let mut conn = DB_POOL.get_conn()?;
+
+    let (query, bindings) = user.create_sql();
+
+    conn.exec_drop(query, bindings)?;
+
     Ok(())
 }
--- src/modules/user.rs
+++ src/modules/user.rs
@@ -1,16 +1,18 @@
+use mysql::{Params, params};
+
 use crate::utils::{
     encryption::{check_password, encrypt},
     xtime::Xtime,
 };
 
 pub struct user {
-    uuid: i32,
-    username: String,
-    password: String,
-    email: String,
-    created_at: String,
-    last_online: String,
-    is_admin: bool,
+    pub uuid: i32,
+    pub username: String,
+    pub password: String,
+    pub email: String,
+    pub created_at: String,
+    pub last_online: String,
+    pub is_admin: bool,
 }
 
 impl user {
@@ -30,5 +32,24 @@ impl user {
 
     pub fn verify_password(&self, password: &str) -> bool {
         check_password(password, &self.password)
+    }
+
+    pub fn create_sql(&self) -> (&'static str, Params) {
+        (
+            "INSERT INTO users (username, password, email, created_at, last_online, is_admin)
+             VALUES (:username, :password, :email, :created_at, :last_online, :is_admin)",
+            params! {
+                "username" => &self.username,
+                "password" => &self.password,
+                "email" => &self.email,
+                "created_at" => &self.created_at,
+                "last_online" => &self.last_online,
+                "is_admin" => self.is_admin,
+            },
+        )
+    }
+
+    pub fn get_uuid(&self) -> i32 {
+        self.uuid
     }
 }
