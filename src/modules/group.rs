35d7c6da82a75e21274110c565bea71b2ff92d09
Alef11 <alex.m.huber@web.de>
2025-05-04 15:55:39 +0200

    Change ints to unsigned ints

--- src/modules/group.rs
+++ src/modules/group.rs
@@ -1,15 +1,15 @@
 use crate::utils::xtime::Xtime;
 
 pub struct Group {
-    pub id: i32,
+    pub id: u32,
     pub name: String,
-    pub owner_id: i32,
+    pub owner_id: u32,
     pub created_at: Xtime,
 }
 
 pub struct GroupMember {
-    pub group_id: i32,
-    pub user_id: i32,
+    pub group_id: u32,
+    pub user_id: u32,
     pub joined_at: Xtime,
     pub is_admin: bool,
 }
--- src/modules/message.rs
+++ src/modules/message.rs
@@ -1,10 +1,10 @@
 use crate::utils::xtime::Xtime;
 
 pub struct Messages {
-    pub umid: i128,
-    pub sender_id: i32,
-    pub recipient_id: Option<i32>,
-    pub group_id: Option<i32>,
+    pub umid: u128,
+    pub sender_id: u32,
+    pub recipient_id: Option<u32>,
+    pub group_id: Option<u32>,
     pub content: String,
     pub sent_at: Xtime,
 }
