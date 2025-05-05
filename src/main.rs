2d46088594236b5fa6ac6592aec16aefba0b29ef
Alef11 <alex.m.huber@web.de>
2025-05-05 23:36:17 +0200

    Add Rocket

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
-fn main() {}
+#[macro_use]
+extern crate rocket;
+
+use rocket::config::{Config, TlsConfig};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+async fn rocket() -> _ {
+    let config = Config {
+        port: 443,
+        address: "0.0.0.0".parse().unwrap(),
+        tls: Some(TlsConfig::from_paths(
+            "certs/certificate.pem",
+            "certs/private.pem",
+        )),
+        ..Config::default()
+    };
+
+    rocket::custom(config).mount("/", routes![index])
+}
